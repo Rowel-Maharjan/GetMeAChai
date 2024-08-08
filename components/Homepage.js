fc86209f19fdfe9a843ba2d1f209b4a41df9d429
Rowel Maharjan <rowelmhj@gmail.com>
2024-08-09 01:23:46 +0545

    Changes in front page

--- components/Homepage.js
+++ components/Homepage.js
@@ -14,34 +14,44 @@ const Homepage = () => {
                 </div>
             </div>
             <div className='h-0.5 bg-white opacity-25'></div>
-            <div className='flex flex-col justify-center h-96 gap-10'>
+            <div className='flex flex-col justify-center h-[72vh] gap-10'>
                 <h1 className='text-3xl major text-center font-bold'>Your Fans Can Buy You A Chai</h1>
                 <div className='flex justify-around'>
-                    <div className='flex flex-col items-center gap-2'>
-                        <div className='relative bg-slate-400 rounded-full p-3 h-32 w-32'>
-                            <img className='absolute top-4 right-6 w-20' src="/tea.gif" alt="Image Not Found" />
+                    <div className='flex flex-col items-center gap-3'>
+                        <img className=' bg-slate-400 rounded-full p-3 h-32 w-32' src="/dollar.gif" alt="Image Not Found" />
+                        <div className='flex flex-col justify-center items-center'>
+                            <p className='font-bold text-2xl'>Fund Yourself</p>
+                            <p>Personal Contribution towards your goal.</p>
                         </div>
-                        <p className='font-bold text-2xl'>Fund Yourself</p>
-                        <p>Personal Contribution towards your goal.</p>
                     </div>
-                    <div className='flex flex-col items-center gap-2'>
-                        <div className='relative bg-slate-400 rounded-full p-3 h-32 w-32'>
-                            <img className='absolute top-4 right-6 w-20' src="/tea.gif" alt="Image Not Found" />
+                    <div className='flex flex-col items-center gap-3'>
+                        <img className=' bg-slate-400 rounded-full p-3 h-32 w-32' src="/target.gif" alt="Image Not Found" />
+                        <div className='flex flex-col justify-center items-center'>
+                            <p className='font-bold text-2xl'>Set Clear Goals</p>
+                            <p>Define your fundraising target.</p>
                         </div>
-                        <p className='font-bold text-2xl'>Fund Yourself</p>
-                        <p>Personal Contribution towards your goal.</p>
                     </div>
-                    <div className='flex flex-col items-center gap-2'>
-                        <div className='relative bg-slate-400 rounded-full p-3 h-32 w-32'>
-                            <img className='absolute top-4 right-6 w-20' src="/tea.gif" alt="Image Not Found" />
+                    <div className='flex flex-col items-center gap-3'>
+                        <img className=' bg-slate-400 rounded-full p-3 h-32 w-32' src="/goal.gif" alt="Image Not Found" />
+                        <div className='flex flex-col justify-center items-center'>
+                            <p className='font-bold text-2xl'>Track Progress</p>
+                            <p>Monitor funds and milestones easily.</p>
                         </div>
-                        <p className='font-bold text-2xl'>Fund Yourself</p>
-                        <p>Personal Contribution towards your goal.</p>
                     </div>
-
                 </div>
             </div>
+            <div className='h-0.5 bg-white opacity-25'></div>
 
+            <div className='flex flex-col justify-center h-[73vh] gap-10 px-36'>
+                <h1 className='text-3xl major text-center font-bold'>Learn More About Us</h1>
+                <div className='flex flex-col gap-5 text-xl'>
+                    <p className='text-center'>Welcome to GetMeAChai, your go-to platform for raising funds and supporting dreams. Easily fund personal projects, help friends reach their goals, and track progress with secure and user-friendly tools.</p>
+
+                    <p className='text-center'>Set clear goals and share your story to engage your community. Define your fundraising targets, connect with supporters through compelling narratives, and inspire them to contribute to your cause.</p>
+
+                    <p className='text-center'>Celebrate milestones and offer rewards to incentivize donations. Track campaign progress, thank your supporters, and encourage generosity by providing meaningful rewards for contributions.</p>
+                </div>
+            </div>
         </>
     )
 }
